d1c75e1df6fa91e51e4af24f2b2ffcdfefd6777b
Seif1234seif1234 <126608064+Seif1234seif1234@users.noreply.github.com>
2024-12-03 20:21:25 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,13 @@
+from function_ import print_
+from numpy import array
+from random import randint
+m = array([[str()]*2]*2)
+print_()
+print("seif".find("e"))
+print("seif"[0:1])
+print("seif"[0:1] + "seif"[2:len("seif")])
+for i in range (2):
+    for j in range (2):
+        m[i,j] = chr(randint(ord("A"),ord("Z")))
+print(m[0,0])
+print("seif"[0:0])
